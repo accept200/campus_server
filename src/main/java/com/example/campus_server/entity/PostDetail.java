0bda55aedf610ead03fb9d6ac25cb56696830173
fang6886 <1461434157@qq.com>
2023-03-26 15:23:29 +0800

    添加了帖子细节

--- src/main/java/com/example/campus_server/entity/PostDetail.java
+++ src/main/java/com/example/campus_server/entity/PostDetail.java
@@ -1,5 +1,13 @@
 package com.example.campus_server.entity;
 
 public class PostDetail extends Post{
+ private String post_content;
 
+    public void setPost_content(String post_content) {
+        this.post_content = post_content;
+    }
+
+    public String getPost_content() {
+        return post_content;
+    }
 }
