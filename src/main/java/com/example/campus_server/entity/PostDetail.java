2bf76c497370c6c3e55f9a4bced7b4cf5daf4cf6
test <a173209@163.com>
2023-03-26 16:47:04 +0800

    add entities

--- src/main/java/com/example/campus_server/entity/PostDetail.java
+++ src/main/java/com/example/campus_server/entity/PostDetail.java
@@ -1,6 +1,6 @@
 package com.example.campus_server.entity;
 
-<<<<<<< HEAD
+
 public class PostDetail extends Post {
     private String content;
 
@@ -10,16 +10,5 @@ public class PostDetail extends Post {
 
     public void setContent(String content) {
         this.content = content;
-=======
-public class PostDetail extends Post{
- private String post_content;
-
-    public void setPost_content(String post_content) {
-        this.post_content = post_content;
-    }
-
-    public String getPost_content() {
-        return post_content;
->>>>>>> 0bda55aedf610ead03fb9d6ac25cb56696830173
     }
 }
