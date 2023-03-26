8efa3b819fbfc85e020b3719904b3e6ce650ead7
fang6886 <1461434157@qq.com>
2023-03-26 14:19:10 +0800

    添加了数据库接口

---
+++ src/main/java/com/example/campus_server/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.example.campus_server.dao.impl;
+
+import com.example.campus_server.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public String helloWord() {
+        return null;
+    }
+}
