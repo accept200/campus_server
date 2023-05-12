4d94ed36f17fee3ae1fa33644a3939456ad03c03
test <a173209@163.com>
2023-05-12 16:27:30 +0800

    完成项目

--- src/main/java/com/example/campus_server/config/LoginConfig.java
+++ src/main/java/com/example/campus_server/config/LoginConfig.java
@@ -1,6 +1,7 @@
 package com.example.campus_server.config;
 
 import com.example.campus_server.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class LoginConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        InterceptorRegistration registration = registry.addInterceptor(new LoginInterceptor());
-        registration.addPathPatterns("**/*.*");
-
+        InterceptorRegistration registration = registry.addInterceptor(loginInterceptor);
+        registration.addPathPatterns("/**");
+        registration.excludePathPatterns("/api/user/login", "/api/user/signup");
     }
 }
--- src/main/java/com/example/campus_server/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/campus_server/interceptor/LoginInterceptor.java
@@ -6,22 +6,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
     private final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
     @Autowired
     private RedisUtil redisUtil;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return true;
-//        String token = request.getHeader("token");
-//        int uid = request.getIntHeader("uid");
-//        if (token == null || token.equals(""))
-//            return false;
-//        else {
-//            int user_id = (int) redisUtil.get(token);
-//            return uid == user_id;
-//        }
+        String token = request.getHeader("token");
+        String uid = request.getHeader("uid");
+        logger.warn("token: {} uid: {}", token, uid);
+        if (token == null || token.isEmpty() || uid == null || uid.isEmpty())
+            return false;
+        else {
+            int user_id = (int) redisUtil.get(token);
+            return Integer.parseInt(uid) == user_id;
+        }
     }
 }
