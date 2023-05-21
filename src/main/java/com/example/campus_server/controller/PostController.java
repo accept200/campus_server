b20926e1a49c4a4b6e637c49be6bded0691a8c8a
test <a173209@163.com>
2023-05-21 17:16:41 +0800

    完成项目

--- src/main/java/com/example/campus_server/controller/PostController.java
+++ src/main/java/com/example/campus_server/controller/PostController.java
@@ -11,6 +11,7 @@ import com.example.campus_server.utils.ResultFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,8 +45,9 @@ public class PostController {
     }
 
     @RequestMapping(value = "newPost")
-    public Result addPost(@RequestHeader Map<String, String> header, NewPost post) {
+    public Result addPost(@RequestHeader Map<String, String> header, @RequestBody NewPost post) {
         int uid = Integer.parseInt(header.get("uid"));
+        logger.warn(post.toString());
         postService.addPost(uid, post);
         return ResultFactory.getSuccessResult("成功");
     }
--- src/main/java/com/example/campus_server/data/NewPost.java
+++ src/main/java/com/example/campus_server/data/NewPost.java
@@ -7,4 +7,46 @@ public class NewPost implements Serializable {
     int stag_id;
     int ttag_id;
     String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStag_id() {
+        return stag_id;
+    }
+
+    public void setStag_id(int stag_id) {
+        this.stag_id = stag_id;
+    }
+
+    public int getTtag_id() {
+        return ttag_id;
+    }
+
+    public void setTtag_id(int ttag_id) {
+        this.ttag_id = ttag_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "NewPost{" +
+                "title='" + title + '\'' +
+                ", stag_id=" + stag_id +
+                ", ttag_id=" + ttag_id +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
